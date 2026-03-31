fa4effe5c242123856ba97f19d79dc2b5ca0808f
AgungTrivaldo <agungtrivaldo@gmail.com>
2026-03-31 23:14:28 +0700

    revised oltp to bronze dag

--- dags/oltp_to_bronze.py
+++ dags/oltp_to_bronze.py
@@ -3,30 +3,25 @@ from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.decorators import dag, task
 from airflow.providers.trino.hooks.trino import TrinoHook
 
+
 TABLE = {
-    "users":{
-        "type":"full",
-        "query":"SELECT * FROM postgres.public.users"
+"users": {
+        "query": "SELECT * FROM postgres.public.users",
+        "watermark": "updated_at",
+        "primary_key": "user_id"
     },
     "orders": {
-        "type":"incremental",
-        "query":"""
+        "query": """
             SELECT 
-                user_id,
-                order_id,
-                CAST(total_amount AS DOUBLE) as total_amount,
-                address_id,
-                order_date,
-                order_status,
-                updated_at
+                user_id, order_id, CAST(total_amount AS DOUBLE) as total_amount,
+                address_id, order_date, order_status, updated_at
             FROM postgres.public.orders
-            """,
-        "watermark":"updated_at",
-        "primary_key":"order_id",
-        "columns":["user_id,order_id,CAST(total_amount AS DOUBLE) as total_amount,address_id,order_date,order_status,updated_at"]
+        """,
+        "watermark": "updated_at",
+        "primary_key": "order_id"
     }
-    
 }
+
 
 @dag(
     dag_id="oltp_to_bronze",
@@ -35,21 +30,35 @@ TABLE = {
     catchup=False,
 )
 def oltp_to_bronze():
+    @task.branch
+    def table_checking(table):
+        trino_hook = TrinoHook(trino_conn_id="trino_conn")
+        sql = f"SHOW TABLES FROM iceberg.bronze LIKE '{table}'"
+        records = trino_hook.get_records(sql)
+        if not records:
+            return f"create_table_{table}"
+        else:
+            return f"upsert_table_{table}"
     @task
-    def incremental(config,table_name,**context):
-        trino_hook =  TrinoHook(trino_conn_id="trino_conn")
-        query = f"SELECT column_name FROM postgres.information_schema.columns WHERE table_schema = 'public' AND table_name = '{table_name}'"
-        records = trino_hook.get_records(query)
+    def create_table(config,table):
+        trino_hook = TrinoHook(trino_conn_id="trino_conn")
+        sql = f"CREATE TABLE iceberg.bronze.{table} AS {config['query']}"
+        trino_hook.run(sql)
+    @task
+    def upsert_table(config,table,**context):
+        trino_hook = TrinoHook(trino_conn_id="trino_conn")
+        get_cols = f"SELECT column_name FROM postgres.information_schema.columns WHERE table_schema = 'public' AND table_name = '{table}'"
+        records = trino_hook.get_records(get_cols)
         cols = [row[0] for row in records]
         primary_key =  config["primary_key"]
-        run_date = context['data_interval_start'].isoformat()
-        update_set = ", ".join([f"{cols} = oltp.{cols}" for cols in cols if cols != primary_key])
+        update_set = ", ".join([f"{col} = oltp.{col}" for col in cols if col != primary_key])
         insert_cols = ", ".join(cols)
-        insert_values = ", ".join([f"oltp.{cols}" for cols in cols])
+        insert_values = ", ".join([f"oltp.{col}" for col in cols])
+        run_date = context['data_interval_start'].isoformat()
         sql = f"""
-                    MERGE INTO iceberg.bronze.orders bronze
+                    MERGE INTO iceberg.bronze.{table} bronze
                     USING (
-                        {config['query']} WHERE {config['watermark']} >= '{{{{ data_interval_start }}}}'
+                        {config['query']} WHERE {config['watermark']} >= '{run_date}'
                     ) oltp
                     ON {primary_key} = oltp.{primary_key}
                     WHEN MATCHED THEN UPDATE SET {update_set}
@@ -58,17 +67,10 @@ def oltp_to_bronze():
         trino_hook.run(sql)
 
     for table, config in TABLE.items():
-        if config["type"] == "full":
-            SQLExecuteQueryOperator(
-                task_id = f"full_load_{table}",
-                conn_id = "trino_conn",
-                sql = f"""
-                    DROP TABLE IF EXIST iceberg.bronze.{table};
-                    CREATE TABLE icberg.bronze.{table} AS {config['query']};
-                """
-        )
-        else:
-            incremental(config=config ,table_name=table)
+        checker = table_checking.override(task_id=f"table_checking_{table}")(table)
+        creator = create_table.override(task_id=f"create_table_{table}")(config,table)
+        upsert = upsert_table.override(task_id=f"upsert_table_{table}")(config,table)
 
+        checker >> [creator,upsert]
 
 oltp_to_bronze()
